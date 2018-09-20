65cdc7e45c9bddfbf6476b9c9606f412d7fade9f
mayank.ku <mayank.ku@BBMDVMAYANKKU.local>
2018-09-20 12:34:36 +0530

    map()

--- src/Hello.java
+++ src/Hello.java
@@ -3,17 +3,20 @@ import java.util.stream.Stream;
 public class Hello {
     public static void main(String[] args) {
         Employee[] employees = {
-                new Employee(1, "Mayank", 2000),
-                new Employee(1, "Arif", 3000),
-                new Employee(1, "Faisal", 4000),
+                new Employee(1, "Employee 1", 2000),
+                new Employee(1, "Employee 2", 3000),
+                new Employee(1, "Employee 3", 4000),
+                new Employee(1, "Employee 4", 5000),
+                new Employee(1, "Employee 6", 6000),
         };
         Stream<Employee> employeeStream = Stream.of(employees);
         System.out.println(employeeStream);
         employeeStream
-                .limit(2)
+                .limit(3)
                 .skip(1)
+                .map(employee -> employee.getName().toUpperCase())
                 .forEach(employee -> {
-                    System.out.println(employee.getName());
+                    System.out.println(employee);
                 });
 
         System.out.println(employeeStream);
